555045277e8c6fc8c2da78387c193b0799425e06
singhrajnish12399-dev <singhrajnish12399@gmail.com>
2026-06-12 16:59:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,26 @@ const products = [
     id: 1,
     name: "iPhone 15",
     price: "₹69,999",
+    image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=500"
   },
   {
     id: 2,
     name: "Gaming Headset",
     price: "₹2,999",
+    image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500"
   },
   {
     id: 3,
     name: "Smart Watch",
     price: "₹4,999",
+    image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500"
   },
   {
     id: 4,
     name: "Laptop",
     price: "₹54,999",
-  },
+    image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?w=500"
+  }
 ];
 function App() {
   return (
