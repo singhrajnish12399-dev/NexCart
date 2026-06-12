7db5f0a0d0f45ac743adbe031c0777df1a4caa8a
singhrajnish12399-dev <singhrajnish12399@gmail.com>
2026-06-12 16:57:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 15",
+    price: "₹69,999",
+  },
+  {
+    id: 2,
+    name: "Gaming Headset",
+    price: "₹2,999",
+  },
+  {
+    id: 3,
+    name: "Smart Watch",
+    price: "₹4,999",
+  },
+  {
+    id: 4,
+    name: "Laptop",
+    price: "₹54,999",
+  },
+];
 function App() {
   return (
     <>
@@ -26,6 +48,15 @@ function App() {
   <div className="category">💻 Electronics</div>
   <div className="category">📚 Books</div>
   <div className="category">🎮 Gaming</div>
+</div>
+<div className="products">
+  {products.map((product) => (
+    <div className="product-card" key={product.id}>
+      <h3>{product.name}</h3>
+      <p>{product.price}</p>
+      <button>Add to Cart</button>
+    </div>
+  ))}
 </div>
       </main>
     </>
